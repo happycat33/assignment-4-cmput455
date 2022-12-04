be3423623bb018bf7b1b71fe56e14516c28eca13
yuihan <juyi@ualberta.ca>
2022-12-04 12:36:28 -0700

    deleted mcts, changed gtp_connection and NoGo4 to assignment 3 and Go4's

--- assignment4/insert_title_here/nogo4.py
+++ assignment4/insert_title_here/nogo4.py
@@ -8,17 +8,19 @@
 
 
 from gtp_connection import GtpConnection
-from board_base import DEFAULT_SIZE, GO_POINT, GO_COLOR
+from board_base import DEFAULT_SIZE, GO_POINT, GO_COLOR, opponent
 from PatternSelection import get_best_move_based_on_pattern
 from board import GoBoard
 from board_util import GoBoardUtil
 from engine import GoEngine
 from ucb import runUcb
+from simulation_engine import GoSimulationEngine
 from board_base import GO_COLOR, GO_POINT, BLACK, WHITE
 
 
-class NoGo:
-    def __init__(self):
+class NoGo(GoSimulationEngine):
+    def __init__(self, sim: int, move_select: str, sim_rule: str, 
+                 check_selfatari: bool, limit: int = 100) -> None:
         """
         Go player that selects moves randomly from the set of legal moves.
         Does not use the fill-eye filter.
@@ -31,7 +33,10 @@ class NoGo:
         version : float
             version number (used by the GTP interface).
         """
-        GoEngine.__init__(self, "NoGo4", 1.0)
+        GoSimulationEngine.__init__(self, "NoGo4", 1.0,
+        sim, move_select, sim_rule, check_selfatari, limit)
+        self.use_ucb = (move_select != "simple")
+        self.use_pattern = (sim_rule == "rulebased")
 
     def simulate(self, board: GoBoard, move: GO_POINT,
                  toplay: GO_COLOR) -> GO_COLOR:
@@ -40,7 +45,12 @@ class NoGo:
         """
         cboard: GoBoard = board.copy()
         cboard.play_move(move, toplay)
-        return self.playGame(cboard)
+        opp = opponent(toplay)
+        return self.playGame(cboard, opp, komi=self.komi,
+            limit=self.args.limit,
+            random_simulation=self.args.sim_rule,
+            use_pattern=self.use_pattern,
+            check_selfatari=self.args.check_selfatari,)
 
     def playGame(self, board: GoBoard) -> GO_COLOR:
         """
@@ -112,6 +122,52 @@ class NoGo:
                 return WHITE
             else:
                 return BLACK
+def parse_args() -> Tuple[int, str, str, bool]:
+    """
+    Parse the arguments of the program.
+    """
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "--sim",
+        type=int,
+        default=10,
+        help="number of simulations per move, so total playouts=sim*legal_moves",
+    )
+    parser.add_argument(
+        "--moveselect",
+        type=str,
+        default="simple",
+        help="type of move selection: simple or ucb",
+    )
+    parser.add_argument(
+        "--simrule",
+        type=str,
+        default="prob",
+        help="type of simulation policy: random or rulebased or prob",
+    )
+    parser.add_argument(
+        "--movefilter",
+        action="store_true",
+        default=False,
+        help="whether use move filter or not",
+    )
+
+    args = parser.parse_args()
+    sim = args.sim
+    move_select = args.moveselect
+    sim_rule = args.simrule
+    move_filter = args.movefilter
+
+    if move_select != "simple" and move_select != "ucb":
+        print("moveselect must be simple or ucb")
+        sys.exit(0)
+    if sim_rule != "random" and sim_rule != "rulebased" and sim_rule != "prob":
+        print("simrule must be random or rulebased or prob")
+        sys.exit(0)
+
+    return sim, move_select, sim_rule, move_filter
 
 
 
@@ -120,9 +176,11 @@ def run() -> None:
     start the gtp connection and wait for commands.
     """
     board: GoBoard = GoBoard(DEFAULT_SIZE)
-    con: GtpConnection = GtpConnection(NoGo(), board)
+    engine: NoGo = NoGo(sim, move_select, sim_rule, check_selfatari)
+    con: GtpConnection = GtpConnection(engine, board)
     con.start_connection()
 
 
 if __name__ == "__main__":
-    run()
+    sim, move_select, sim_rule, check_selfatari = parse_args()
+    run(sim, move_select, sim_rule, check_selfatari)
